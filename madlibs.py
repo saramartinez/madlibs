6bf1f8e020a6aa3f5f56d384313d1770266281b6
Hackbright Student <info@hackbrightacademy.com>
2015-01-29 16:32:44 -0800

    second commit

--- madlibs.py
+++ madlibs.py
@@ -23,12 +23,17 @@ def bye():
 # madlib route to play game
 @app.route('/madlib')
 def show_madlib():
+    print request.args
     name = request.args.get("username")
     person = request.args.get("person")
     color = request.args.get("color")
     adjective = request.args.get("adjective")
     noun = request.args.get("noun")
-    return render_template("madlib.html", username = name, person = person, noun = noun, adjective = adjective, color = color)
+    justin = request.args.get("justin")
+    direction = request.args.get("direction")
+    feelings = request.args.get("feelings")
+    verb = request.args.get("verb")
+    return render_template("madlib.html", username = name, person = person, noun = noun, adjective = adjective, color = color, justin = justin, direction = direction, feelings = feelings, verb = verb)
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
